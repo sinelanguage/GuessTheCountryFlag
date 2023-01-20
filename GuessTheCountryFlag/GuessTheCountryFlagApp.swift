81f508fc84ea3dac4f5205064aa7589beca94727
Adam Winick <sinelanguage@gmail.com>
2023-01-19 20:42:20 -0500

    Initial Commit

---
+++ GuessTheCountryFlag/GuessTheCountryFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheCountryFlagApp.swift
+//  GuessTheCountryFlag
+//
+//  Created by Adam Winick on 2023-01-19.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheCountryFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
