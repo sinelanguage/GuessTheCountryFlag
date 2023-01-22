e3a3ad96ea0dd8d23314ed2b915799ed37401475
Adam Winick <sinelanguage@gmail.com>
2023-01-21 21:44:51 -0500

    material and background blur

--- GuessTheCountryFlag/ContentView.swift
+++ GuessTheCountryFlag/ContentView.swift
@@ -9,13 +9,30 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+//        VStack(alignment: .leading, spacing: 20) {
+//            Text("Hello, world!")
+//            Text("Hello, world2!")
+//
+//            HStack(alignment: .top , spacing: 20) {
+//                Text("Hello, world!")
+//                Text("Hello, world2")
+//                Text("Hello, world2")
+//            }
+//        }
+        ZStack {
+            VStack(spacing: 0) {
+                Color.white
+                Color.blue
+            }
+            Text("Sylvia")
+                .foregroundStyle(.secondary)
+                .padding(50)
+                .background(.ultraThinMaterial)
         }
-        .padding()
+        .ignoresSafeArea()
+        // this won't fill the whole view
+        // z stack works like z index
+        // .background(.red)
     }
 }
 
